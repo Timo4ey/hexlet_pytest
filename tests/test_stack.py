4441dca6c669dff8a3871fd0469ca07288a1ed08
Timofey <tima_yakovishin@mail.ru>
2023-02-27 10:59:21 +0300

    upd

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,12 +1,13 @@
 import pytest
 
 
+
 def test_stack1():
     stack = []
     stack.append('one')
     stack.append('two')
 
-assert stack.pop() == 'two'
+    assert stack.pop() == 'two'
 
 
 def test_stack2():
